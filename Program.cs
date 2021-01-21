2ca33449b92294fb47c0dbbc6cb4ac1685b24652
anil.kumar <anil329@gmail.com>
2021-01-20 22:21:04 -0500

    Added Utilities Services, having function to write log into log.txt file

--- Program.cs
+++ Program.cs
@@ -4,18 +4,24 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WorkerService5._0.Services;
 
 namespace WorkerService5._0
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureServices((hostContext, services) => { services.AddHostedService<Worker>(); });
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<Worker>();
+                    services.AddSingleton<UtilitiesService>();
+
+                }).UseWindowsService();
     }
 }
---
+++ Services/UtilitiesService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Hosting;
+
+namespace WorkerService5._0.Services
+{
+    public class UtilitiesService
+    {
+        private readonly IHostEnvironment _env;
+        private const string LogFileName = "log.txt";
+        
+        public UtilitiesService(IHostEnvironment env)
+        {
+            _env = env ?? throw new ArgumentNullException(nameof(env));
+        }
+
+        public void WriteLog(string text)
+        {
+            var filePath = Path.Combine(_env.ContentRootPath, LogFileName);
+            if (!File.Exists(filePath))
+            {
+                using var myFile = File.Create(filePath);
+                myFile.Dispose();
+            }
+            var writer = new StreamWriter(filePath, true);
+            writer.WriteLine("DateTime: " + DateTimeOffset.Now.ToString("F"));
+            writer.WriteLine(text);
+            writer.WriteLine("*********************************************************");
+            writer.Close();
+        }
+    }
+}
